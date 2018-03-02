bdd7fdcc9f03fea00152dd5b6e75a6612bb2d9be
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-03-02 21:51:40 +0100

    Add worker that reads reminder times from DB and sends them to users

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -7,9 +7,40 @@ import (
 
 	"github.com/kgantsov/stockholm_commute_bot/pkg/client"
 	"github.com/kgantsov/stockholm_commute_bot/pkg/handlers"
+	"github.com/kgantsov/stockholm_commute_bot/pkg/models"
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
+
+func worker(app *handlers.App) {
+	c := app.Session.DB("commute_bot").C("users")
+
+	ticker := time.NewTicker(time.Minute * 1)
+
+	for t := range ticker.C {
+		var users []models.User
+		c.Find(bson.M{"home_time": t.Format(time.Kitchen)}).All(&users)
+
+		for _, user := range users {
+			trips := app.Sl.GetHomeTrips(user)
+
+			for _, trip := range trips.Trip {
+				app.Bot.Send(&tb.Chat{ID: user.ChatID}, app.Sl.GetMessageForTrip(trip), tb.ModeMarkdown)
+			}
+		}
+
+		c.Find(bson.M{"work_time": t.Format(time.Kitchen)}).All(&users)
+
+		for _, user := range users {
+			trips := app.Sl.GetWorkTrips(user)
+
+			for _, trip := range trips.Trip {
+				app.Bot.Send(&tb.Chat{ID: user.ChatID}, app.Sl.GetMessageForTrip(trip), tb.ModeMarkdown)
+			}
+		}
+	}
+}
 
 func main() {
 	session, err := mgo.Dial(os.Getenv("MONGODB_URLS"))
@@ -41,6 +72,8 @@ func main() {
 	bot.Handle("/set_home_reminder", handlers.SetHomeReminderHandler(&app))
 	bot.Handle("/set_work", handlers.SetWorkHandler(&app))
 	bot.Handle("/set_work_reminder", handlers.SetWorkReminderHandler(&app))
+
+	go worker(&app)
 
 	bot.Start()
 }
