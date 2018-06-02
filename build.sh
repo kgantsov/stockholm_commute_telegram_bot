690cbe40be5208276e52f47aa524bf91e4659ef8
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-06-02 19:46:09 +0200

    Remove build step from build.sh since we build go app inside docker

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ TAG=latest
 USER="kgantsov"
 DOCKER_ID_USER="kgantsov"
 
-cd cmd/bot && GOOS=linux GIN_MODE=release go build && cd ../..
-
 docker build -t $USER/$NAME:$TAG --no-cache .
 
 docker tag $USER/$NAME:$TAG $USER/$NAME:$TAG
